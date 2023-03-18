d74eeaf875ad114f4b2ae456ebb4ba07360f9ba4
mdArshad10 <mdarshd246@gmail.com>
2023-03-17 23:44:58 -0400

    add to the setup of top-50 books

--- index.py
+++ index.py
@@ -1,7 +1,10 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-import time
+import pickle
+from sklearn.metrics.pairwise import cosine_similarity
+from PIL import Image
+
 
 st.set_page_config(layout='wide',page_title='Home | Book Recommendation')
 
@@ -11,15 +14,57 @@ with st.spinner('Wait for it...'):
 
     tab1, tab2, tab3 = st.tabs(["Home", "Top-50 Books", "Recommended Books"])
 
-    with tab1:
-        st.header("A cat")
-        st.image("https://static.streamlit.io/examples/cat.jpg", width=200)
-
+    #! top - 50 Table
     with tab2:
-        st.header("A dog")
-        st.image("https://static.streamlit.io/examples/dog.jpg", width=200)
+        i = 0
+        while(i<50):
+            col1, col2, col3, col4 = st.columns(4)
+            with col1: 
+                # book - image 
+                st.image("http://images.amazon.com/images/P/0439136350.01.LZZZZZZZ.jpg", width=200)
+                # Book Title
+                st.text(i)
+                i=i+1
+                st.text('Harry Potter and the Prisoner of Azkaban') 
+                # Book Author
+                st.text('J. K. Rowling')
+                st.text("Avg. rating: 5.852804")
+                
+            with col2: 
+                # book - image 
+                st.image("http://images.amazon.com/images/P/0439136350.01.LZZZZZZZ.jpg", width=200)
+                # Book Title
+                st.text(i)
+                i=i+1
+                st.text('Harry Potter and the Prisoner of Azkaban') 
+                # Book Author
+                st.text('J. K. Rowling')
+                st.text("Avg. rating: 5.852804")
+            
+            with col3: 
+                # book - image 
+                st.image("http://images.amazon.com/images/P/0439136350.01.LZZZZZZZ.jpg", width=200)
+                # Book Title
+                st.text(i)
+                i=i+1
+                st.text( 'Harry Potter and the Prisoner of Azkaban') 
+                # Book Author
+                st.text('J. K. Rowling')
+                st.text("Avg. rating: 5.852804")
 
-    with tab3:
-        st.header("An owl")
-        st.image("https://static.streamlit.io/examples/owl.jpg", width=200)
+            with col4: 
+                # book - image 
+                st.image("http://images.amazon.com/images/P/0439136350.01.LZZZZZZZ.jpg", width=200)
+                # Book Title
+                st.text(i)
+                i=i+1
+                st.text('Harry Potter and the Prisoner of Azkaban') 
+                # Book Author
+                st.text('J. K. Rowling')
+                # Avg Rating
+                st.text("Avg. rating: 5.852804")
+            
+
+
+
 
